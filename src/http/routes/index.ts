649fa357a39432b7144b0287e9d344f0ac7c7498
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-03-18 14:55:09 -0300

    feat: enable request id

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,8 +1,11 @@
 import { InternalServerError } from '@/errors/internal-server-error'
 import { Hono } from 'hono'
+import { requestId } from 'hono/request-id'
 import { statusRoutes } from './status'
 
 export const routes = new Hono().basePath('v1')
+
+routes.use(requestId())
 
 routes.route('/status', statusRoutes)
 
