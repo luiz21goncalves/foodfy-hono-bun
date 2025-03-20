6dbc33e79b0020deff96febc751d59e3eda56caf
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2025-03-20 14:03:08 -0300

    ci: set database container name

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,7 +4,7 @@ import { db } from '@/api/db/connection'
 
 beforeAll(async () => {
   execSync(
-    `docker compose exec database sh -c "createdb -h ${process.env.POSTGRES_HOST} -p ${process.env.POSTGRES_PORT} -U ${process.env.POSTGRES_USER} ${process.env.POSTGRES_DB}"`
+    `docker exec foodfy-hono-bun-postgres-dev sh -c "createdb -h ${process.env.POSTGRES_HOST} -p ${process.env.POSTGRES_PORT} -U ${process.env.POSTGRES_USER} ${process.env.POSTGRES_DB}"`
   )
   execSync('bun run migrate')
 })
@@ -12,6 +12,6 @@ beforeAll(async () => {
 afterAll(async () => {
   await db.$client.end()
   execSync(
-    `docker compose exec database sh -c "dropdb -h ${process.env.POSTGRES_HOST} -p ${process.env.POSTGRES_PORT} -U ${process.env.POSTGRES_USER} ${process.env.POSTGRES_DB}"`
+    `docker exec foodfy-hono-bun-postgres-dev sh -c "dropdb -h ${process.env.POSTGRES_HOST} -p ${process.env.POSTGRES_PORT} -U ${process.env.POSTGRES_USER} ${process.env.POSTGRES_DB}"`
   )
 })
